e5bbc8b7f2eaba10a5af992c7addaec6440e4084
Adam Kirkwood <hello@adamkirkwood.com>
2017-07-13 20:50:04 -0700

    :lipstick: Adjusting grid

--- pages/work/sketchpacks/index.js
+++ pages/work/sketchpacks/index.js
@@ -62,7 +62,7 @@ export default class extends React.Component {
             </div>
           </section>
 
-          <blockquote className={'o-container o-container--wide u-pv-ultra'}>
+          <blockquote className={'o-container o-container--wide u-pv-mega'}>
             <p className={'o-type-big-quote u-weight-light o-type-italic u-color-quote u-mb-small'}>
               &ldquo;I&apos;m super excited about @sketchpacks, that was a much needed tool!&rdquo;
             </p>
@@ -71,45 +71,104 @@ export default class extends React.Component {
             </strong>
           </blockquote>
 
-          <section className={'o-container u-pv-mega o-container--centered'}>
+          <section className={'o-container u-pv-mega'}>
             <div className={'o-grid'}>
-              <div className={'o-grid__col u-push-1/4 u-pull-1/4'}>
+              <div className={'o-grid__col u-1/1 u-1/4@sm'}>
                 <h2 className={'o-type-display-1 u-color-headline'}>The Approach</h2>
-
+              </div>
+              <div className={'o-grid__col u-1/1 u-3/4@sm'}>
                 <p className={'o-type-body-2 u-color-body-1'}>
-                  I worked closely with users and developers to understand
-                  the essential interconnected elements of their systems,
-                  which resulted in a simple and effortless product.
+                  Designing products that provide essential services must have
+                  designed systems to support it. Well-designed services resolve
+                  friction by using interconnected systems arranged to meet
+                  desired outcomes.
                 </p>
               </div>
             </div>
           </section>
 
-          <section className={'o-container u-pb-mega o-container--centered'}>
+          <section className={'o-container u-pv-mega'}>
             <div className={'o-grid'}>
-              <div className={'o-grid__col u-1/2'}>
-                <h2 className={'o-type-display-1 u-color-headline'}>Analyze</h2>
+              <div className={'o-grid__col col-1/1 u-1/2 c-card'}>
+                <div className={'u-mb-small u-uppercase u-weight-bold'}>Developer</div>
 
-                <p className={'o-type-body-2 u-color-body-1'}>
-                  Service blueprints help surface potential experience gaps and
-                  areas for improvement.
+                <p>
+                  When I have committed my final changes,<br />
+                  I want to publish my work,<br />
+                  So that my users will receive my latest revisions.
                 </p>
               </div>
 
-              <div className={'o-grid__col u-1/2'}>
-                <h2 className={'o-type-display-1 u-color-headline'}>Synthesize</h2>
+              <div className={'o-grid__col col-1/1 u-1/2 c-card'}>
+                <strong>Developer</strong>
 
-                <p className={'o-type-body-2 u-color-body-1'}>
-                  Reducing each system reveals their elements, interconnections,
-                  and purpose.
+                <p>
+                  When my plugin has been published,<br />
+                  I want to view its analytics,<br />
+                  So that I can measure its performance.
                 </p>
               </div>
             </div>
           </section>
+
+
+
+          <section className={'o-container o-container--wide u-pb-mega'}>
+            <div className={'o-grid o-grid--wide'}>
+              <div className={'o-grid__col u-1/1 u-1/3@sm'}>
+                <h2 className={'o-type-title u-color-headline'}>Uncover insights and opportunities</h2>
+
+                <p className={'o-type-body-2 u-color-body-1'}>
+                  Service blueprints are a great method for documenting and analyzing
+                  the user&apos;s journey within an ecosystem. The map surfaced
+                  points of friction and areas of opportunity that could be solved
+                  with a well&ndash;designed system in place.
+                </p>
+              </div>
+
+              <div className={'o-grid__col u-1/1 u-2/3@sm u-bgcolor-tango'}>
+                Insert image here
+              </div>
+            </div>
+          </section>
+
+          <section className={'o-container o-container--wide u-pb-mega'}>
+            <div className={'o-grid o-grid--rev'}>
+              <div className={'o-grid__col u-1/3@sm'}>
+                <h2 className={'o-type-title u-color-headline'}>Map the ecosystem and its forces</h2>
+
+                <p className={'o-type-body-2 u-color-body-1'}>
+                  Every system in place has a purpose. Whether or not they are
+                  efficient and obvious is a different story.
+                  To understand each system&apos;s purpose, I began to reduce
+                  them just their essential elements and their interconnections.
+                </p>
+              </div>
+
+              <div className={'o-grid__col u-2/3@sm u-bgcolor-tango'}>
+                Insert image here
+              </div>
+            </div>
+          </section>
+
+          <blockquote className={'o-container o-container--wide u-pv-mega'}>
+            <p className={'o-type-big-quote u-weight-light o-type-italic u-color-quote u-mb-small'}>
+              &ldquo;The beauty of a living thing is not the atoms that go into
+              it, but the way those atoms are put together.&rdquo;
+            </p>
+            <strong className={'u-uppercase o-type-small u-color-quote'}>
+              Carl Sagan
+            </strong>
+          </blockquote>
 
           <section className={'o-container u-pv-mega o-container--centered'}>
             <div className={'o-grid'}>
               <div className={'o-grid__col'}>
+                <img
+                  src="/static/work/sketchpacks/app@2x.png"
+                  width="720"
+                  className={'u-mb-large'} />
+
                 <h2 className={'o-type-display-1 u-color-headline'}>Download Sketchpacks for free</h2>
 
                 <p className={'o-type-body-2 u-color-body-1 u-mb-large'}>
