724a3413cc958f603b13700aa95cfed435ad89d2
Adam Kirkwood <hello@adamkirkwood.com>
2017-07-14 20:51:37 -0700

    :construction: JobStoryCard update

--- pages/work/sketchpacks/index.js
+++ pages/work/sketchpacks/index.js
@@ -32,30 +32,32 @@ export default class extends React.Component {
 
         <article className={'l-case-study'}>
 
-          <section className={'o-container o-container--wide u-pt-ultra u-pb-large'}>
-            <header className={'o-grid o-grid--wide'}>
-              <div className={'o-grid__col u-1/2'}>
-                <h1 className={'o-type-promo u-mb-medium'}>Sketchpacks</h1>
-                <p className={'o-type-lead u-color-headline u-mb-medium'}>
-                  A macOS app that empowers Sketch users to manage and sync
-                  their plugins across machines and teams.
-                </p>
-                <a href={'https://sketchpacks.com/'} className={'c-button c-button--primary'}>
-                  Download Sketchpacks
-                </a>
-              </div>
+          <div className={'u-angled--bottom o-page-header'}>
+            <section className={'o-container o-container--wide u-pt-ultra u-pb-large'}>
+              <header className={'o-grid o-grid--wide'}>
+                <div className={'o-grid__col u-1/2'}>
+                  <h1 className={'o-type-promo u-mb-medium'}>Sketchpacks</h1>
+                  <p className={'o-type-lead u-color-headline u-mb-medium'}>
+                    A macOS app that empowers Sketch users to manage and sync
+                    their plugins across machines and teams.
+                  </p>
+                  <a href={'https://sketchpacks.com/'} className={'c-button c-button--primary'}>
+                    Download Sketchpacks
+                  </a>
+                </div>
 
-              <div className={'o-grid__col u-1/2'}>
-                <img
-                  src="/static/work/sketchpacks/app@2x.png"
-                  width="740"
-                  className={'u-mb-large'}
-                />
-              </div>
-            </header>
-          </section>
+                <div className={'o-grid__col u-1/2'}>
+                  <img
+                    src="/static/work/sketchpacks/app@2x.png"
+                    width="740"
+                    className={'u-mb-large'}
+                  />
+                </div>
+              </header>
+            </section>
+          </div>
 
-          <section className={'o-container u-pv-mega'}>
+          <section className={'o-container u-pt-ultra u-pb-mega'}>
             <div className={'o-grid'}>
               <div className={'o-grid__col u-1/1 u-1/4@sm'}>
                 <h2 className={'o-type-display-1 u-color-headline'}>The Challenge</h2>
@@ -140,17 +142,66 @@ export default class extends React.Component {
           </section>
 
           <section className={'o-container o-container--wide u-pv-mega'}>
-            <div className={'o-grid o-grid--wide'}>
-              <div className={'o-grid__col col-1/1 u-1/2@sm'}>
+            <div className={'o-grid o-grid--wide o-grid--cards'}>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-mb-large u-text-center@sm'}>
+                <h2 className={'o-type-title u-color-headline u-mb-small'}>Job Stories</h2>
+
+                <p className={'o-type-body-2 u-color-body-1 u-push-1/4@sm u-pull-1/4@sm'}>
+                  Choosing which problems to solve for creates focus and intentionality.
+                  I curated which problems I want to tackle while taking note of
+                  their interconnected systems and touchpoints.
+                </p>
+              </div>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
                 <JobStoryCard
+                  archetype={'User'}
+                  actor={'When I want to automate reptitive tasks within Sketch'}
+                  action={'I want to search for a qualified plugin'}
+                  outcome={'So that I can be more efficient'}
+                />
+              </div>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
+                <JobStoryCard
+                  archetype={'User'}
+                  actor={'When I become aware of new versions of plugins'}
+                  action={'I want to update them'}
+                  outcome={'So that I can receive new features and bug fixes'}
+                />
+              </div>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
+                <JobStoryCard
+                  archetype={'User'}
+                  actor={'When I work across multiple machines regularly'}
+                  action={'I want access to the same plugins'}
+                  outcome={'So that I can preserve my design tool chain'}
+                />
+              </div>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
+                <JobStoryCard
+                  archetype={'User'}
+                  actor={'When I am need to share my set of plugins'}
+                  action={'I want provide my list of plugins'}
+                  outcome={'So that the recipient can install my set of plugins'}
+                />
+              </div>
+
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
+                <JobStoryCard
+                  archetype={'Developer'}
                   actor={'When I have committed my final changes'}
                   action={'I want to publish my work'}
                   outcome={'So that my users will receive my latest revisions'}
                 />
               </div>
 
-              <div className={'o-grid__col col-1/1 u-1/2@sm'}>
+              <div className={'o-grid__col o-grid--equal-height u-1/1 u-1/2@sm'}>
                 <JobStoryCard
+                  archetype={'Developer'}
                   actor={'When my plugin has been published'}
                   action={'I want to view its analytics'}
                   outcome={'So that I can measure its performance'}
